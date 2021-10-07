0b94c48fb866ea15a36c7491fd553fc8230f5cb0
haritshammam <siegkounat@gmail.com>
2021-10-07 17:53:21 +0800

    added homepage, searchbar, and categories components with it's corresponding styling behavior and make it responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SearchbarComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
